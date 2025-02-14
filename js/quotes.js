21dc7a96317180c565b42dac4833447286a3fc87
umersanii <iamumersani@gmail.com>
2025-02-14 16:53:14 +0500

    1.04
    Minor adjustments

--- js/quotes.js
+++ js/quotes.js
@@ -1,6 +1,19 @@
 // Quotes data directly in JavaScript
 const quotes = [
-    { "quote": "There is no sun, there is no glory, there is no victory. But it's all in one.", "author": "David Goggins" }
+    { "quote": "There is no sun, there is no glory, there is no victory. But it's all in one.", "author": "David Goggins"
+    ,"quote": "Discipline will hurt you for a moment. Laziness will haunt you for a lifetime", "author": "Unknown"
+    ,"quote": "A fool prays for an easier life. A warrior prays for the strength to endure a difficult one.", "author": "Bruce Lee"
+    ,"quote": "Without Allah I am nothing but a road to my own distruction" , "author": "Unknown"
+    ,"quote": "Does the stone realise every strike of the hammer and chisel is shaping it into a masterpiece", "author": "Unknown"
+    ,"quote": "If there's no shoulder to cry on, there's definitely a floor to kneel and pray on", "author": "Unknown"
+    ,"quote": "It's abscene will teach you more than it's presence ever could", "author": "Unknown"
+    ,"quote": "You can't wish for both a strong character and an easy life. The price of each, is the other.", "author": ""
+    ,"quote": "a head full of fears has no space for dreams", "author": ""
+    ,"quote": "When small men begin to cast big shadwos, it means the sun is about to set", "author": "Lin Yutang"
+    ,"qupte": "The man who is a master of patience is a master of everything else", "author": "George Savile"
+    ,"quote": "Discipline will hurt you for a moment. Laziness will haunt you for a lifetime", "author": "Unknown"    
+     
+}
 ];
 
 function displayRandomQuote() {
